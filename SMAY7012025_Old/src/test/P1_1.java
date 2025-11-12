86903adbc7af8fc4c172c83cd0cea409a00cf192
Vivek <Vivek@DESKTOP-LJLC1JG>
2025-11-12 23:19:21 +0530

    New Commit - Synchronization

--- SMAY7012025_Old/src/test/P1_1.java
+++ SMAY7012025_Old/src/test/P1_1.java
@@ -11,28 +11,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class P1_1 {
 
 	public static void main(String[] args) {
-		//Open Browser, Maximize it and Navigate to URL
 		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(2, TimeUnit.SECONDS);
 		
-		driver.get("https://ibegin.tcsapps.com/candidate/register");
+		driver.get("https://trello.com/");
 
-//		Thread.sleep(5000);
-
-		driver.findElement(By.xpath("//input[contains(@placeholder,'skills')]")).sendKeys("Testing");
-
-//		Thread.sleep(3000);
-
-		List<WebElement> allAutoSuggetions = driver.findElements(By.xpath("//li[@data-ng-repeat='item in itemList']"));
-		
-		for (WebElement ele : allAutoSuggetions) {
-			System.out.println(ele.getText());
-		}
-		
-		driver.quit();
+		driver.findElement(By.xpath("//a[text()='Log in' and @data-uuid]")).click();
 	}
 
 }
